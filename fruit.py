b595e57c24efd80c332c4e33d779b9376ca5ff84
leafar-tb <leafar1@gmx.net>
2017-10-05 17:14:22 +0200

    added stems
    plus misc tweaks

--- fruit.py
+++ fruit.py
@@ -5,8 +5,8 @@ import random
 
 from math import pi as PI
 from mathutils import Vector
-from .spline import HermiteInterpolator, screw
-from .util import optional, makeDiffuseMaterial, MeshMerger
+from .spline import HermiteInterpolator, screw, bevelCircle
+from .util import optional, makeDiffuseMaterial, MeshMerger, clip
 from .evolution import Evolvable
 from .notnum import linspace
 
@@ -31,7 +31,7 @@ class FlowerResidueProps:
     fr_radius = bpy.props.FloatProperty(
         name        = "Flower Residue Position",
         description = "Where to place the FR along the fruit",
-        default     = .2,
+        default     = .02,
         min         = 0,
         soft_max    = .05
     )
@@ -39,7 +39,7 @@ class FlowerResidueProps:
     fr_length = bpy.props.FloatProperty(
         name        = "Flower Residue Length",
         description = "Length of the FR, relative to fruit size",
-        default     = .2,
+        default     = .05,
         min         = 0,
         soft_min    = 0.01, soft_max     = .1
     )
@@ -52,22 +52,58 @@ class FlowerResidueProps:
         soft_max    = 13
     )
 
-class FruitProperties(FlowerResidueProps):
-        
-    radius = bpy.props.FloatProperty(
-        name        = "Width",
-        description = "Width of the fruit",
-        default     = .2,
-        min         = 0,
-        soft_min    = 0.01, soft_max     = .5
+#############################################
+
+class StemProps:
+    
+    stem_present = bpy.props.BoolProperty(
+        name        = "Add stem",
+        description = "Whether to add a stem to the model",
+        default     = True
     )
+    
+    stem_radius = bpy.props.FloatProperty(
+        name        = "Stem Radius",
+        description = "Determines where the stem meets the fruit and thus also the radius",
+        default     = .01,
+        min         = 0,
+        soft_min    = .005, soft_max    = .02
+    )
+    
+    stem_length = bpy.props.FloatProperty(
+        name        = "Stem Length",
+        description = "Length of the stem, relative to fruit length",
+        default     = .15,
+        min         = 0,
+        soft_min    = 0.1, soft_max     = .25
+    )
+    
+    stem_bending = bpy.props.FloatProperty(
+        name        = "Stem Bending",
+        description = "How strongly the stem bends sideways",
+        default     = 0,
+        min         = 0,
+        soft_max    = 1
+    )
+
+#############################################
+
+class FruitProperties(FlowerResidueProps, StemProps):
+        
+    #radius = bpy.props.FloatProperty(
+    #    name        = "Width",
+    #    description = "Width of the fruit",
+    #    default     = .2,
+    #    min         = 0,
+    #    soft_min    = 0.01, soft_max     = .5
+    #)
     
     length = bpy.props.FloatProperty(
         name        = "Length",
         description = "Length of the fruit",
         default     = .5,
         min         = 0,
-        soft_min    = 0.05, soft_max     = 1
+        soft_min    = 0.2, soft_max     = 1
     )
     
     symmetry = bpy.props.IntProperty(
@@ -103,7 +139,7 @@ class FruitProperties(FlowerResidueProps):
         default     = (0, 0, 0),
         min         = 0, max = 1
     )
-
+    
 #############################################
 
 class Fruit(Evolvable, FruitProperties):
@@ -123,17 +159,12 @@ class Fruit(Evolvable, FruitProperties):
     def _outerSpline(self):
         def angle2Vec(a):
             return Vector((math.cos(a), 0, math.sin(a)))
-        
         x = 0, self.length
         y = Vector((0, 0, self.length)), Vector((0, 0, 0))
         dy = angle2Vec(self.upperAngle), angle2Vec(self.lowerAngle + PI)
-                
         return HermiteInterpolator(x, y, dy)
     
     def _makeFlowerResidue(self):
-        if not self.fr_present:
-            return [], []
-        
         petals = self.fr_petals
         rPoint = self._outerSpline()((1-self.fr_radius)*self.length)
         vertices = [rPoint, rPoint.copy(), rPoint.copy() - Vector((0, 0, self.fr_length*self.length))]
@@ -148,6 +179,18 @@ class Fruit(Evolvable, FruitProperties):
         faces = [ (i, i+1, i+2) for i in range(0, 3*petals, 3)]
         return vertices, faces
 
+    def _buildStem(self):
+        radius, zbase = self._outerSpline()(self.stem_radius).xz
+        length = self.length*self.stem_length
+        
+        x = 0, length
+        y = Vector((0, 0, zbase)), Vector((length*self.stem_bending, 0, zbase+length))
+        angle = clip(self.stem_bending, 0, 1)*PI/2 # at the upper stem
+        dy = Vector((0, 0, 1)), Vector((math.sin(angle), 0, math.cos(angle)))
+        
+        stem = HermiteInterpolator(x, y, dy)
+        return bevelCircle(stem, radius, closeEnds=True)
+
     def toMesh(self, LOD=16):
         """
         Create a bpy.data.meshes instance from this Evolvable. Here you define how the properties translate into a mesh.
@@ -157,9 +200,14 @@ class Fruit(Evolvable, FruitProperties):
         """
         
         mm = MeshMerger()
-        mm.add(*screw(self._outerSpline(), LODr=10, LODp=LOD, rScale=lambda a: (math.sin(self.symmetry*a)+2)/2), 
-             makeDiffuseMaterial(self.colour))
-        mm.add(*self._makeFlowerResidue(), makeDiffuseMaterial([.12,.06,0]))
+        mm.add(*screw(self._outerSpline(), LODr=10, LODp=max(LOD, self.symmetry*4), rScale=lambda a: math.sin(self.symmetry*a)/4 + 1), 
+            makeDiffuseMaterial(self.colour))
+        
+        brown = [.12,.06,0] # use a fixed color for now
+        if self.fr_present:
+            mm.add(*self._makeFlowerResidue(), makeDiffuseMaterial(brown))
+        if self.stem_present:
+            mm.add(*self._buildStem(), makeDiffuseMaterial(brown))
         
         return mm.buildMesh("Fruit")
 
